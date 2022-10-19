4a06e5dd098bb415b56056359a4365729f1bb693
黄容鑫 <2675471454@qq.com>
2022-10-19 17:26:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '温大可爱',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqUhz6ABbKBSfGRTig2tcnOpYLMo',
+      id: 'oqUhz6FCTyQ0f9A6rQJsecd8xrw4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'eVV0l-H93i4UKti7z8wMwvl-MwV2xqrKYA4jh_5feec',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

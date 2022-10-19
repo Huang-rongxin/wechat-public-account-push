ca4248cf1a1e513872f696e88bd6a1878ff088c5
黄容鑫 <2675471454@qq.com>
2022-10-19 16:25:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqUhz6ABbKBSfGRTig2tcnOpYLMo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	dqxOlag59m_z_ihmqZLGI3YX_gdU3jU6LW2uz-WbW78',
+      useTemplateId: 'dqxOlag59m_z_ihmqZLGI3YX_gdU3jU6LW2uz-WbW78',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-04',
       festivals: [

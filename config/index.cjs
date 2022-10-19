813c59941b05383568f85072cc75bc2914135025
黄容鑫 <2675471454@qq.com>
2022-10-19 16:41:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,10 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqUhz6ABbKBSfGRTig2tcnOpYLMo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dqxOlag59m_z_ihmqZLGI3YX_gdU3jU6LW2uz-WbW78',
+      useTemplateId: 'FpHq0SqofYmsaf9i8IjzKx8-Mq1VrNR26MD4_Ui1Guc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-04',
+      horoscopeDateType: '今日',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
